afe137718ce12b17a78ed1f82da735eb98d72184
Brightchamp <brightchamp5877@gmail.com>
2022-07-30 15:33:27 +0530

    Added mongoDB connection Database under the account of Brightchamp

--- server/index.js
+++ server/index.js
@@ -2,11 +2,21 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+const mongoose = require('mongoose');
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 const port = process.env.PORT || 5000;
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { dbName:'PostalManagement', useNewUrlParser: true });
+
+const connection = mongoose.connection;
+connection.once('open',()=>{
+    console.log('MongoDB connection established successfully!')
+})
 
 app.listen(port,()=>{
     console.log(`Server running on port ${port}`);
